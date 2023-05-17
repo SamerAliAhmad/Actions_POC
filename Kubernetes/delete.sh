6bc219633775850113e22445d6178283e99cadd3
samer.ahmad <sameraliahmad01@gmail.com>
2023-05-17 15:46:23 +0400

    test aws

--- Kubernetes/delete.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws eks --region me-central-1 update-kubeconfig --name districtnex-cluster
-
-rm -R Helm cleanup.sh skaffold.sh skaffold.yaml uninstall.sh upgrade.sh setup_cluster.sh delete.sh
